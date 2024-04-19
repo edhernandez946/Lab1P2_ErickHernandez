97d866edfaa3ae1b83dfeef6758363369e58aca1
Daniel Hernandez <edhernandez946@gmail.com>
2024-04-19 14:53:59 -0600

    empezado bubblesort

--- src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
+++ src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
@@ -1,3 +1,5 @@
+//Nombre: Erick Hernandez, Fila 1, Silla 7
+
 package lab1p2_erickhernandez;
 
 import java.util.ArrayList;
@@ -8,30 +10,40 @@ public class Lab1P2_ErickHernandez {
     
     static Random random = new Random();
     static Scanner sc = new Scanner (System.in);
+    static int tamaño = tamañoMatriz();
+    
     
     public static int [][] matrizRandom (int tamaño) {
         
-        int [][] matrizRandom = new int [tamaño][tamaño];
+        int [][] temporal = new int [tamaño][tamaño];
         
         for (int i = 0; i < tamaño; i++) {
             for (int j = 0; j < tamaño; j++) {
-                matrizRandom [i][j] = random.nextInt(0,10);
+                temporal [i][j] = random.nextInt(0,10);
             }
         }
         
-        return matrizRandom;
+        return temporal;
         
     }
+    
     
     public static int tamañoMatriz () {
         
-        System.out.println("Ingrese el tamaño de la matriz");
-        int tamaño = sc.nextInt();
-        return tamaño;
+        System.out.println("Ingrese el tamaño de la matriz (mayor que 4 e impar");
+        
+        int temporal = sc.nextInt();
+        while (temporal <= 4 || temporal % 2 == 0) {
+            System.out.println("El numero tiene que ser mayor a 4 e impar)");
+            temporal = sc.nextInt();
+        }
+        
+        return temporal;
         
     }
     
-    public static void imprimir(int[][] matriz) {
+    
+    public static void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + " ");
@@ -41,14 +53,50 @@ public class Lab1P2_ErickHernandez {
         System.out.println("");
     }
     
+    
+    public static void imprimirArray(int[] array) {
+        
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j] + " ");
+            }
+        }
+    
+    
+    public static int [][] bubbleSort (int [][] matrizRandom) {
+        
+        int [][] temporal = new int [tamaño][tamaño];
+        temporal = matrizRandom;
+        
+        int [][] matrizOrdenada = new int [tamaño][tamaño];
+        
+        int [] fila = new int [tamaño];
+        
+        for (int i = 0 ; i < tamaño ; i++) {
+            for (int j = 0 ; j < tamaño ; j++) {
+                fila [j] = temporal [i][j];
+                
+            }
+        }
+        
+        imprimirArray (fila);
+        
+        
+        
+        return matrizOrdenada;
+        
+    }
+   
+    
     public static void main(String[] args) {
         
-        int tamaño = tamañoMatriz();
         int [][] matriz = new int [tamaño][tamaño];
-        
         matriz = matrizRandom(tamaño);
+        ArrayList <Integer> medianas = new ArrayList<>();
         
-        imprimir (matriz);
+        imprimirMatriz(matriz);
+        
+        bubbleSort(matriz);
+        
         
     }
     
