cdce7fa5d36439c37353a10dc29c399cbb6ed334
Daniel Hernandez <edhernandez946@gmail.com>
2024-04-19 14:31:52 -0600

    funciones para matriz random

--- src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
+++ src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1p2_erickhernandez;
 
-/**
- *
- * @author edher
- */
-public class Lab1P2_ErickHernandez {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Lab1P2_ErickHernandez {
+    
+    static Random random = new Random();
+    static Scanner sc = new Scanner (System.in);
+    
+    public static int [][] matrizRandom (int tamaño) {
+        
+        int [][] matrizRandom = new int [tamaño][tamaño];
+        
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                matrizRandom [i][j] = random.nextInt(0,10);
+            }
+        }
+        
+        return matrizRandom;
+        
+    }
+    
+    public static int tamañoMatriz () {
+        
+        System.out.println("Ingrese el tamaño de la matriz");
+        int tamaño = sc.nextInt();
+        return tamaño;
+        
+    }
+    
+    public static void imprimir(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int tamaño = tamañoMatriz();
+        int [][] matriz = new int [tamaño][tamaño];
+        
+        matriz = matrizRandom(tamaño);
+        
+        imprimir (matriz);
+        
     }
     
 }
