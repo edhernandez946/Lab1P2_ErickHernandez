034c9515c575c85e4f0f3cd91e384556a1d717f8
Daniel Hernandez <edhernandez946@gmail.com>
2024-04-19 16:14:15 -0600

    bubblesort parcialmente terminado

--- src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
+++ src/lab1p2_erickhernandez/Lab1P2_ErickHernandez.java
@@ -1,5 +1,4 @@
 //Nombre: Erick Hernandez, Fila 1, Silla 7
-
 package lab1p2_erickhernandez;
 
 import java.util.ArrayList;
@@ -7,42 +6,39 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Lab1P2_ErickHernandez {
-    
+
     static Random random = new Random();
-    static Scanner sc = new Scanner (System.in);
+    static Scanner sc = new Scanner(System.in);
     static int tamaño = tamañoMatriz();
-    
-    
-    public static int [][] matrizRandom (int tamaño) {
-        
-        int [][] temporal = new int [tamaño][tamaño];
-        
+
+    public static int[][] matrizRandom(int tamaño) {
+
+        int[][] temporal = new int[tamaño][tamaño];
+
         for (int i = 0; i < tamaño; i++) {
             for (int j = 0; j < tamaño; j++) {
-                temporal [i][j] = random.nextInt(0,10);
+                temporal[i][j] = random.nextInt(0, 10);
             }
         }
-        
+
         return temporal;
-        
+
     }
-    
-    
-    public static int tamañoMatriz () {
-        
+
+    public static int tamañoMatriz() {
+
         System.out.println("Ingrese el tamaño de la matriz (mayor que 4 e impar");
-        
+
         int temporal = sc.nextInt();
         while (temporal <= 4 || temporal % 2 == 0) {
             System.out.println("El numero tiene que ser mayor a 4 e impar)");
             temporal = sc.nextInt();
         }
-        
+
         return temporal;
-        
+
     }
-    
-    
+
     public static void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
@@ -52,52 +48,63 @@ public class Lab1P2_ErickHernandez {
         }
         System.out.println("");
     }
-    
-    
+
     public static void imprimirArray(int[] array) {
-        
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[j] + " ");
-            }
+
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
         }
-    
-    
-    public static int [][] bubbleSort (int [][] matrizRandom) {
-        
-        int [][] temporal = new int [tamaño][tamaño];
+    }
+
+    public static int[][] bubbleSort(int[][] matrizRandom) {
+
+        int[][] temporal = new int[tamaño][tamaño];
         temporal = matrizRandom;
-        
-        int [][] matrizOrdenada = new int [tamaño][tamaño];
-        
-        int [] fila = new int [tamaño];
-        
-        for (int i = 0 ; i < tamaño ; i++) {
-            for (int j = 0 ; j < tamaño ; j++) {
-                fila [j] = temporal [i][j];
-                
+
+        int[][] matrizOrdenada = new int[tamaño][tamaño];
+
+        int[] fila = new int[tamaño];
+
+        for (int i = 0; i < tamaño; i++) {
+
+            for (int j = 0; j < tamaño; j++) {
+                fila[j] = temporal[i][j];
             }
+
+            for (int k = 0; k < tamaño; k++) {
+                for (int l = k; l < tamaño - 1; l++) {
+
+                    if (fila [k] > fila [l+1]) {
+
+                        int temp = fila [k];
+                        fila [k] = fila [l + 1];
+                        fila [l + 1] = temp;
+
+                    }
+
+                }
+
+            }
+
+            imprimirArray(fila);
+            System.out.println("");
+
         }
-        
-        imprimirArray (fila);
-        
-        
-        
+
         return matrizOrdenada;
-        
+
     }
-   
-    
+
     public static void main(String[] args) {
-        
-        int [][] matriz = new int [tamaño][tamaño];
+
+        int[][] matriz = new int[tamaño][tamaño];
         matriz = matrizRandom(tamaño);
-        ArrayList <Integer> medianas = new ArrayList<>();
-        
+        ArrayList<Integer> medianas = new ArrayList<>();
+
         imprimirMatriz(matriz);
-        
+
         bubbleSort(matriz);
-        
-        
+
     }
-    
+
 }
